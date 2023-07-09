2e439b70db9ba8ffcd4976087c75473f5cb62680
JurasicShift <64592484+JurasicShift@users.noreply.github.com>
2023-07-09 10:59:19 +0100

    remove uppercase

--- index.js
+++ index.js
@@ -5,14 +5,10 @@ const __dirname = path.resolve();
 
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.use('/', express.static(path.join(__dirname, 'views')));
 app.use('/styles', express.static(__dirname + '/styles'));
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/js', express.static(__dirname + '/js'));
-
-app.get('/', (req, res) => {
-	res.render('/views/index.html');
-});
 
 app.listen(port, err => {
 	if (err) console.log('Error in server setup:', err);
