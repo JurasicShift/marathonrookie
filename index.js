987f6322835f26b22b4a9c7f01cf4757c3487f0d
JurasicShift <64592484+JurasicShift@users.noreply.github.com>
2023-07-14 14:23:35 +0100

    added route

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const __dirname = path.resolve();
 
 const port = process.env.PORT || 8080;
 
-app.use('/', express.static(path.join(__dirname, '/views')));
+app.use( express.static(path.join(__dirname)));
 app.use('/styles', express.static(__dirname + '/styles'));
 app.use('/img', express.static(__dirname + '/img'));
 app.use('/js', express.static(__dirname + '/js'));
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/views/index.html'));
+});
 
 app.listen(port, err => {
 	if (err) console.log('Error in server setup:', err);
